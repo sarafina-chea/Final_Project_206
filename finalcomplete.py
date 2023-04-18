fe81dffb774e3b2ecb73d033c999b8d4483be2b7
sarafina-chea <123130516+sarafina-chea@users.noreply.github.com>
2023-04-17 20:32:59 -0400

    Add files via upload

---
+++ finalcomplete.py
@@ -0,0 +1,15 @@
+#FINAL COMPLETE FUNCTION LIST
+
+import requests
+import random
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+import nltk
+import unittest
+import sqlite3
+import json
+import re
+import os
+from datetime import date
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
